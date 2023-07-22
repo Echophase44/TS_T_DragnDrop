df1766fbb64ebd829c8244c4b4debd834d98d9f7
Nick <Echophase44@github.com>
2023-07-21 21:30:24 -0400

    Building out the ProjectInput class more.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
@@ -10,7 +13,44 @@ class ProjectInput {
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = 'user-input'
+
+    this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
+
+    this.configure()
     this.attach()
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if(
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim.length === 0
+    ) {
+      alert("Invalid input, try again!")
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople]
+    }
+
+  }
+
+  private submitHandler(event: Event){
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if(Array.isArray(userInput)){
+      const [title, desc, people] = userInput;
+      
+    }
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this))
   }
 
   private attach() {
